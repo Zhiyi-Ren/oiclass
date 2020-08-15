05e9997a7827cf084c2ba9479fc82f4755824ebf
Zhiyi Ren <ren-zhiyi@outlook.com>
2020-08-15 12:48:39 +0800

    Change

--- 4/test-b.cpp
+++ 4/test-b.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main(void)
 {
-    int n,sum=0;
+    int n,sum=1;
     cin>>n;
-    for (int i=1;i<=n;i++)
-        sum+=i;
-    cout<<sum*sum<<endl;
+    for(int i=1;i<=n;i+=2)
+        sum*=i;
+    cout<<sum<<endl;
     return 0;
 }
